424a138e241684907d45987f555ea1c49e31f44c
Shirshendu Bhowmick <shirshendu.bhowmick3@gmail.com>
2021-10-14 19:26:17 +0530

    fix types

--- src/constants/apiMap.ts
+++ src/constants/apiMap.ts
@@ -1,16 +1,24 @@
-const httpMethods = {
+import { HttpMethods } from '~/services/networkServices';
+
+const httpMethods: { [key in HttpMethods]: HttpMethods } = {
   GET: 'GET',
   POST: 'POST',
   PUT: 'PUT',
   PATCH: 'PATCH',
   DELETE: 'DELETE',
+  HEAD: 'HEAD',
 };
 
 const endpoint = {
   SESSION: '/user/session',
 };
 
-const apiMap = {
+const apiMap: {
+  [key: string]: {
+    endpoint: string;
+    method: HttpMethods;
+  };
+} = {
   CREATE_SESSION: {
     endpoint: endpoint.SESSION,
     method: httpMethods.POST,
