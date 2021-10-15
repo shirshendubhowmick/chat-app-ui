0435b66c157bdf740f395e2e4b971bac586e0936
Shirshendu Bhowmick <shirshendu.bhowmick3@gmail.com>
2021-10-15 12:40:04 +0530

    added size variant

--- src/components/Avatar/Avatar.tsx
+++ src/components/Avatar/Avatar.tsx
@@ -8,12 +8,17 @@ export interface AvatarProps {
   lightBorder: boolean;
   className?: string;
   styleName?: string;
+  size?: 'normal' | 'small';
 }
 
 function Avatar(props: AvatarProps) {
   return (
     <span
-      styleName={classnames('container', { 'light-border': props.lightBorder })}
+      styleName={classnames(
+        'container',
+        { 'light-border': props.lightBorder },
+        `size-${props.size}`,
+      )}
       className={props.className}
       style={{ backgroundImage: `url(${userMap[props.avatarId].avatar})` }}
     />
@@ -23,6 +28,7 @@ function Avatar(props: AvatarProps) {
 Avatar.defaultProps = {
   lightBorder: false,
   className: '',
+  size: 'normal',
 };
 
 export default Avatar;
