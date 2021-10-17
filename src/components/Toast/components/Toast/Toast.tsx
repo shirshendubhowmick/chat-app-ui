5a32069ff51a3a99aa2df21def6fc5f96d52d8e3
Shirshendu Bhowmick <shirshendu.bhowmick3@gmail.com>
2021-10-17 14:19:09 +0530

    added toast component

---
+++ src/components/Toast/components/Toast/Toast.tsx
@@ -0,0 +1,16 @@
+import './Toast.css';
+
+export interface ToastProps {
+  type?: 'error' | 'success';
+  children: JSX.Element;
+}
+
+function Toast(props: ToastProps) {
+  return <div styleName={`container ${props.type}`}>{props.children}</div>;
+}
+
+Toast.defaultProps = {
+  type: 'success',
+};
+
+export default Toast;
