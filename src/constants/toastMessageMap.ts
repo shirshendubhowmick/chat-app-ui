5155e18d5ee5ac2920d891b6b6cfc059bf4c085b
Shirshendu Bhowmick <shirshendu.bhowmick3@gmail.com>
2021-10-18 16:27:27 +0530

    update toast msg map

--- src/constants/toastMessageMap.ts
+++ src/constants/toastMessageMap.ts
@@ -9,13 +9,15 @@ const toastMessageMap = {
     ADMIN_POSITION_NO_LONGER_AVAILABLE:
       'Admin user position is no longer available, joined as a viewer',
     ERROR_LOADING_HISTORICAL_MSGS: 'Unable to load earlier messages',
-    LOGOUT_ERROR: 'Error logging out,please retry',
+    LOGOUT_ERROR: 'Error logging out, please retry',
     ERROR_CHECKING_STATUS: 'Error checking chat service status',
     SOCKET_DISCONNECTED: 'Chat server disconnected',
     SOCKET_CONNECTION_ERRORED:
       'Error establshing socket connection to chat server',
     ERROR_SENDING_MESSAGE: 'Error sending chat messages',
     MAX_FILE_LIMIT_REACHED: 'The file should to be less than 500 KB',
+    LOGGEDOUT_DUPLICATE_TAB:
+      'You are logged out as you have opened the app in another tab',
   },
 };
 
